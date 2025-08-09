a054e0ee12dd9d1c9a4c6da31658ed8816fb0dd7
deogratias228 <gwoblesse@gmail.com>
2025-08-09 14:15:31 +0000

    target _blank

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return <div className="py-8 flex justify-between" >
         <Logo />
         <div className="space-x-6 justify-between">
-            <a href="https://deowoblesse.tech" className='hover:text-[#34D399] hover:font-semibold'>© Gratias Tech 2025</a>
+            <a href="https://deowoblesse.tech" target='_blank' className='hover:text-[#34D399] hover:font-semibold'>© Gratias Tech 2025</a>
             <a href="/" className='hover:text-[#34D399] hover:font-semibold'>Politique de confidentialité</a>
             <a href="/" className='hover:text-[#34D399] hover:font-semibold'>Politique des cookies</a>
             <a href="/" className='hover:text-[#34D399] hover:font-semibold'>Conditions d’utilisation</a>
